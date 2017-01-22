10054e77612b7273ce02dd331a4e3cb56ce6a5cf
Nikhil Srivastava <niksrc@gmail.com>
2017-01-22 19:56:06 +0530

    switch to bytewise encoding

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,12 +4,15 @@ const { resolve } = require('path');
 const levelup = require('levelup');
 const leveldown = require('leveldown');
 const sublevel = require('level-sublevel');
+const bytewise = require('bytewise');
 
 const location = resolve(__dirname, '../db');
 
-const db = sublevel(levelup({
-	db: leveldown(location),
+const db = sublevel(levelup(location, {
+	db: leveldown,
 	cacheSize: 50 * 1024 * 1024, // 50 mb lru read cache
+	valueEncoding: 'json',
+	keyEncoding: bytewise,
 }));
 
 module.exports = db;
